f9980a746595ccffd384046861f31f9ef95a20f0
lorysmutti <lorys.mutti@studenti.unitn.it>
2023-10-19 19:48:51 +0200

    Aggiunto pulsante mostra password

--- Client Utility/ClientUtility.cs
+++ Client Utility/ClientUtility.cs
@@ -8,45 +8,45 @@ namespace Frontend.Client_Utility;
 
 public class ClientUtility
 {
-	private readonly HttpClient _httpClient;
-	private readonly RedBoxBackEndSettings _options;
-	private GrpcChannel _channel;
+    private readonly HttpClient _httpClient;
+    private readonly RedBoxBackEndSettings _options;
+    private GrpcChannel _channel;
 
-	public ClientUtility(IOptions<RedBoxBackEndSettings> options)
-	{
-		_options = options.Value;
-		_httpClient = new HttpClient(new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler()));
-		_channel = GrpcChannel.ForAddress(_options.BackEndUrl, new GrpcChannelOptions
-		{
-			HttpClient = _httpClient
-		});
-	}
+    public ClientUtility(IOptions<RedBoxBackEndSettings> options)
+    {
+        _options = options.Value;
+        _httpClient = new HttpClient(new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler()));
+        _channel = GrpcChannel.ForAddress(_options.BackEndUrl, new GrpcChannelOptions
+        {
+            HttpClient = _httpClient
+        });
+    }
 
-	public GrpcChannel GetChannel()
-	{
-		return _channel;
-	}
+    public GrpcChannel GetChannel()
+    {
+        return _channel;
+    }
 
-	public void SetAuthToken(string token)
-	{
-		_httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(token);
-		_channel = GrpcChannel.ForAddress(_options.BackEndUrl, new GrpcChannelOptions
-		{
-			HttpClient = _httpClient
-		});
-	}
+    public void SetAuthToken(string token)
+    {
+        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(token);
+        _channel = GrpcChannel.ForAddress(_options.BackEndUrl, new GrpcChannelOptions
+        {
+            HttpClient = _httpClient
+        });
+    }
 
-	public void UnsetAuthToken()
-	{
-		_httpClient.DefaultRequestHeaders.Authorization = null;
-		_channel = GrpcChannel.ForAddress(_options.BackEndUrl, new GrpcChannelOptions
-		{
-			HttpClient = _httpClient
-		});
-	}
+    public void UnsetAuthToken()
+    {
+        _httpClient.DefaultRequestHeaders.Authorization = null;
+        _channel = GrpcChannel.ForAddress(_options.BackEndUrl, new GrpcChannelOptions
+        {
+            HttpClient = _httpClient
+        });
+    }
 
-	public bool IsLoggedIn()
-	{
-		return _httpClient.DefaultRequestHeaders.Authorization is not null;
-	}
+    public bool IsLoggedIn()
+    {
+        return _httpClient.DefaultRequestHeaders.Authorization is not null;
+    }
 }
