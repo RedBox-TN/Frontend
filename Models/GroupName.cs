958376bcd49425fdef9dfc21851bd21ce91eb0a9
lorysmutti <lorys.mutti@studenti.unitn.it>
2023-12-22 23:35:43 +0100

    Aggiunti gruppi

---
+++ Models/GroupName.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Frontend.Models;
+
+public class GroupName
+{
+    [Required(ErrorMessage = "Campo obbligatorio")]
+    public string? groupName { get; set; }
+}
