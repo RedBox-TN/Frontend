bc2efc1a1a1e6d2cdea7822b20d2ca5d0ec43070
lorysmutti <lorys.mutti@studenti.unitn.it>
2023-12-24 22:04:03 +0100

    nomi utente nei gruppi

--- Client Utility/ClientUtility.cs
+++ Client Utility/ClientUtility.cs
@@ -1,39 +1,37 @@
-using Blazored.SessionStorage;
 using Frontend.Channel_Utility;
-using Frontend.Token_Utility;
 using Grpc.Health.V1;
 
 namespace Frontend.Client_Utility;
 
 public class ClientUtility
 {
-	private readonly ChannelUtility _channelUtility;
-	private readonly Health.HealthClient _client;
+    private readonly ChannelUtility _channelUtility;
+    private readonly Health.HealthClient _client;
 
 
-	public ClientUtility(ChannelUtility channelUtility)
-	{
-		_channelUtility = channelUtility;
-		_client = new Health.HealthClient(_channelUtility.GetChannel());
-	}
+    public ClientUtility(ChannelUtility channelUtility)
+    {
+        _channelUtility = channelUtility;
+        _client = new Health.HealthClient(_channelUtility.GetChannel());
+    }
 
-	public async Task<bool> IsLoggedIn()
-	{
-		return await Task.Run(() => _channelUtility.GetHttpClient().DefaultRequestHeaders.Authorization != null);
-	}
+    public async Task<bool> IsLoggedIn()
+    {
+        return await Task.Run(() => _channelUtility.GetHttpClient().DefaultRequestHeaders.Authorization != null);
+    }
 
-	public async Task<bool> IsServerOn()
-	{
-		try
-		{
-			var response = await _client.CheckAsync(new HealthCheckRequest());
-			var status = response.Status;
-			return status == HealthCheckResponse.Types.ServingStatus.Serving;
-		}
-		catch (Exception e)
-		{
-			Console.WriteLine(e.Message);
-			return false;
-		}
-	}
+    public async Task<bool> IsServerOn()
+    {
+        try
+        {
+            var response = await _client.CheckAsync(new HealthCheckRequest());
+            var status = response.Status;
+            return status == HealthCheckResponse.Types.ServingStatus.Serving;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return false;
+        }
+    }
 }
